04d8198ff91447bb91237efb4dd9d2fe98311679
JediKirk <darkpasik@gmail.com>
2020-04-29 23:44:21 +0300

    kipit jo

---
+++ src/main/java/dao/entity/Role.java
@@ -0,0 +1,17 @@
+package dao.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "role")
+public class Role extends BaseEntity{
+    @Column(name = "phone_number_id")
+    private Long phoneNumberId;
+    @Column(name = "access_right")
+    private String accessRight;
+}
---
+++ src/main/java/dao/entity/User.java
@@ -0,0 +1,17 @@
+package dao.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "user")
+public class User extends BaseEntity{
+    @Column(name = "phone_number")
+    private int phoneNumber;
+    @Column(name = "password")
+    private String password;
+}
