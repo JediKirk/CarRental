c0b842bcd139ef73b59736be80f920bde9705d96
JediKirk <darkpasik@gmail.com>
2020-05-12 20:39:30 +0300

    Refactor

--- src/main/java/dao/entity/Car.java
+++ src/main/java/dao/entity/Car.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.List;
 
 
 @Data
@@ -12,23 +13,37 @@ import java.time.LocalDate;
 public class Car extends BaseEntity {
     @Column(name = "registered_car_number")
     private String registeredCarNumber;
+
     @Column(name = "body_number")
     private String bodyNumber;
+
     @Column(name = "engine_type")
     private String engineType;
+
     @Column(name = "year_of_issue")
     private LocalDate yearOfIssue;
+
     @Column(name = "brand")
     private String brand;
+
     @Column(name = "model")
     private String model;
+
     @Column(name = "mileage")
     private int mileage;
+
     @Column(name = "price_auto")
     private int priceAuto;
+
     @Column(name = "rental_day_price")
     private int rentalDayPrice;
+
     @ManyToOne
     @JoinColumn(name = "car_class_id")
     private CarClass carClass;
+
+    @OneToMany(mappedBy = "car")
+    private List<RentalInformation> rentalInformation;
+
+
 }
--- src/main/java/dao/entity/PickUpLocation.java
+++ src/main/java/dao/entity/PickUpLocation.java
@@ -2,14 +2,23 @@ package dao.entity;
 
 import lombok.Data;
 
-import javax.persistence.Embedded;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 @Data
 @Entity
-@Table(name="pick_up_location")
-public class PickUpLocation extends  BaseEntity{
+@Table(name = "pick_up_location")
+public class PickUpLocation extends BaseEntity {
     @Embedded
     private Address address;
+
+    @OneToMany(mappedBy = "pickUpLocationId")
+    private List<RentalInformation> rentalInformation;
+
+    @ManyToMany
+    @JoinTable(name = "car_pick_up_location",
+            joinColumns = @JoinColumn(name = "car_id"),
+            inverseJoinColumns = @JoinColumn(name = "pick_up_location_id")
+    )
+    private List<Car> cars;
 }
